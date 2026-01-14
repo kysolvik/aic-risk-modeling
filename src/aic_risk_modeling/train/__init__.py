23ab4ae3367bf536e69baef0137b1cd18f8046f0
Kylen Solvik <kysolvik@gmail.com>
2026-01-14 12:40:05 -0500

    Models module for building keras models

--- src/aic_risk_modeling/train/__init__.py
+++ src/aic_risk_modeling/train/__init__.py
@@ -3,6 +3,13 @@
 Utilities for building dataset and training models
 """
 
+from .models import (
+    get_unet,
+    get_unet_lite,
+    get_mlp,
+    get_multi_scale_mlp_head,
+)
+
 from .data_loader import (
     infer_schema_from_gcs,
     schema_to_feature_spec,
@@ -15,4 +22,8 @@ __all__ = [
     "schema_to_feature_spec",
     "build_features_dict",
     "dataset_from_gcs",
+    "get_unet",
+    "get_unet_lite",
+    "get_mlp",
+    "get_multi_scale_mlp_head",
 ]
