43f76533b5274df7b5e765ec62498d0ba3f3bda5
Kylen Solvik <kysolvik@gmail.com>
2026-01-20 17:05:04 -0500

    Vertex train with spot vm + gpu

--- scripts/00_train_vertex.py
+++ scripts/00_train_vertex.py
@@ -8,8 +8,8 @@ gcs_data_dir='gs://aic-fire-amazon/results_2024_5k/'
 tfrecord_pattern='*.tfrecord.gz'
 output_band='BurnDate'
 patch_size=128
-batch_size=4
-model_output_path='gs://aic-fire-amazon/models/vertex_model_{model_type}.keras'
+batch_size = 4
+model_output_path = f'gs://aic-fire-amazon/models/vertex_model_{model_type}.keras'
 
 
 
@@ -25,13 +25,14 @@ job = aiplatform.CustomPythonPackageTrainingJob(
     display_name=f"fire-risk-model",
     python_package_gcs_uri="gs://aic-fire-amazon/python_packages/aic_risk_modeling-0.0.1.tar.gz",
     python_module_name="aic_risk_modeling.train.train_model",
-    container_uri="us-docker.pkg.dev/vertex-ai/training/tf-cpu.2-16.py310:latest",
+    container_uri="us-docker.pkg.dev/vertex-ai/training/tf-gpu.2-16.py310:latest",
 )
 
 job.run(
     machine_type="n1-highmem-4",
+    scheduling_strategy=aiplatform.compat.types.custom_job.Scheduling.Strategy.SPOT,
     accelerator_type="NVIDIA_TESLA_T4",
-    accelerator_count=0,
+    accelerator_count=1,
     args=[
         f"--model_type={model_type}",
         f"--gcs_data_dir={gcs_data_dir}",
