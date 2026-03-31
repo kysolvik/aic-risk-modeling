f699325f972b9ebb50ea90462b9b41be8bd84c22
Kylen Solvik <kysolvik@gmail.com>
2026-03-31 15:30:46 -0400

    Bump version to 0.1.0, fusion network setup isn't fully backwards compatible with old without some adaptions

--- scripts/train_vertex.py
+++ scripts/train_vertex.py
@@ -1,6 +1,8 @@
 
 from google.cloud import aiplatform
 import argparse
+import google
+
 
 parser = argparse.ArgumentParser()
 parser.add_argument('config_json',
@@ -12,9 +14,9 @@ parser.add_argument('display_name',
 args = parser.parse_args()
 
 # Basic parameters
-project='ksolvik-misc'
+project = google.auth.default()[1]           
 location='us-east1'
-bucket='aic-fire-amazon'
+bucket='res-id'
 config_json=args.config_json
 display_name=args.display_name
 
@@ -26,7 +28,7 @@ aiplatform.init(project=project, location=location, staging_bucket=bucket)
 # https://cloud.google.com/python/docs/reference/aiplatform/latest/google.cloud.aiplatform.CustomTrainingJob
 job = aiplatform.CustomPythonPackageTrainingJob(
     display_name=args.display_name,
-    python_package_gcs_uri="gs://aic-fire-amazon/python_packages/aic_risk_modeling-0.0.4.tar.gz",
+    python_package_gcs_uri="gs://aic-fire-amazon/python_packages/aic_risk_modeling-0.1.0.tar.gz",
     python_module_name="aic_risk_modeling.train.train_model",
     container_uri="us-docker.pkg.dev/vertex-ai/training/tf-gpu.2-16.py310:latest",
 )
