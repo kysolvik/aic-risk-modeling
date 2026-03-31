f67ab14f92aed1bf43cd650f388462220669e0b9
Kylen Solvik <kysolvik@gmail.com>
2026-03-31 15:08:45 -0400

    Rename decoder

--- src/aic_risk_modeling/train/models.py
+++ src/aic_risk_modeling/train/models.py
@@ -214,7 +214,7 @@ def get_identity(input_shape, input_name):
 
     return model
 
-def fusion_decoder(branch_models):
+def decoder_fusion(branch_models):
     """
     branch_models: List of Keras models (e.g., [lstm_branch1, lstm_branch2, cnn_branch])
     """
